51606242155ff66a72eec690b4eac35548cc60fb
Edy Galdiolo <edygaldy302@gmail.com>
2023-04-19 17:36:55 +0200

    Feat(main): added alts and buttons
    (added a small easter egg)

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // --- CONSTANTS ---
 
 const images = ["./img/01.webp", "./img/02.webp", "./img/03.webp", "./img/04.webp", "./img/05.webp"];
+const alts = ["Spiderman", "Ratchet & Clank", "You are lucky that the image didn't load", "Stray", "Avengers"];
 
 // --- VARIABLES ---
 
@@ -13,6 +14,9 @@ do {
 } while (i > 4)
 
 document.getElementById("image").src = images[i];
+document.getElementById("image").alt = alts[i];
+
+
 
 // --- FUNCTIONS ---
 
